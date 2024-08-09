7db4bedf10e2357c153fac8586a1f785c6351bd4
Denis Protopopov <protdenden@gmail.com>
2024-08-09 14:50:16 +0300

    cleaning :)

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 // script.js
-import { Novel } from './Novel/Novel.js';
+import { Novel } from './Novel/Novel.js'
 
-const vnData = '/Novel.json';
-const novel = new Novel("#textbox", '#optionsbox', "#namebox span", vnData);
+const vnData = '/Novel.json'
+const novel = new Novel("#textbox", '#optionsbox', "#namebox span", vnData)
 
-novel.initialize();
+novel.initialize()
