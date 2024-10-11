845e8111ac1b04bcf3f2a68e9376b49fe1909650
Denis Protopopov <protdenden@gmail.com>
2024-10-11 16:50:36 +0400

    remove robot

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default {
     {
         outDir: '../dist', // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
-        sourcemap: false // Add sourcemap
+        sourcemap: true // Add sourcemap
     },
     plugins:
     [
